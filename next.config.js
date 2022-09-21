54d3431bc8f23c614be653452aed5b80018a2d68
ridakaddir <ridakaddir@gmail.com>
2022-09-21 12:41:59 -0400

    inital commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+    register: true,
+    skipWaiting: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+});
+
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
